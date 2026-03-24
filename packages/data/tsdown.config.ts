79fc48db432b5dc78db0327817ec854e7a4ba40b
jan-kubica <jankubica@protonmail.com>
2026-03-24 16:45:31 +0100

    chore: add tsdown to anonymize and anonymize-data

---
+++ packages/data/tsdown.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: ["dictionaries/index.ts"],
+  format: ["esm"],
+  dts: { resolve: true },
+  clean: true,
+  sourcemap: true,
+  hash: false,
+});
